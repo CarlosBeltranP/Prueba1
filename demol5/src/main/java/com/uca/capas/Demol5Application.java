102abe3dbb65583c686965c05f8001e1d4b3c407
CarlosBeltranP <00507511@uca.edu.sv>
2020-05-13 10:41:55 -0600

    base

---
+++ demol5/src/main/java/com/uca/capas/Demol5Application.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demol5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demol5Application.class, args);
+	}
+
+}
